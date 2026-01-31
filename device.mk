25285b40264d6064192cd0bf70f3db960eec118f
Justin Lin <61717681+justinlin099@users.noreply.github.com>
2026-01-31 07:56:56 +0100

    Remove libCB_symlink64 and libCB_symlink32
    Removed unnecessary symlink libraries from device.mk.

--- device.mk
+++ device.mk
@@ -28,8 +28,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-    libCB_symlink64 \
-    libCB_symlink32
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
