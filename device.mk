cfa6546c3ab32e09f68a61bc5c53a1864187c720
Justin Lin <61717681+justinlin099@users.noreply.github.com>
2026-02-12 20:41:20 +0100

    Add Settings Google

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     device/sony/lilac/overlay/packages/apps/Settings\
-    device/sony/yoshino-common/overlay/frameworks/base/packages/SystemUI
+    device/sony/yoshino-common/overlay/frameworks/base/packages/SystemUI\
+    device/sony/lilac/overlay/packages/apps/SettingsGoogle
 
 ### POWER
 TARGET_USE_CUSTOM_POWERHINT := true
